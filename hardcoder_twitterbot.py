42f0b1ba2578dafa4f83dd7d5b3dc67409820d65
Dhanshree Meher <40516815+hardcoder98@users.noreply.github.com>
2020-09-20 13:31:45 +0530

    Create hardcoder_twitterbot.py

---
+++ hardcoder_twitterbot.py
@@ -0,0 +1,36 @@
+import tweepy
+import os
+
+def create_api(): 
+  consumer_key = os.getenv('consumer_key')
+  consumer_secret = os.getenv('consumer_secret')
+  access_token = os.getenv('access_token')
+  access_token_secret = os.getenv('access_token_secret')
+
+  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_token, access_token_secret)
+
+  api = tweepy.API(auth, wait_on_rate_limit= True , wait_on_rate_limit_notify = True)
+  api.verify_credentials()
+  print('API created')
+  return api
+  
+import time
+def follower_count(user):
+  emoji_numbers = {0:"0️⃣",1:"1️⃣",2:"2️⃣",3:"3️⃣",4:"4️⃣",5:"5️⃣",6:"6️⃣",7:"7️⃣",8:"8️⃣",9:"9️⃣"}
+
+  uf_split = [int(i) for i in str(user.followers_count)]
+
+  emoji_followers = ''.join([emoji_numbers[j] for j in uf_split if j in emoji_numbers.keys()])
+  return emoji_followers
+
+api = create_api()
+
+
+while True:
+  user = api.get_user('inmovidu1')
+  print(user)
+  api.update_profile(name=f'INMOVIDU|{follower_count(user)} Followers')
+  print(f'Updating Twitter Name : INMOVIDU|{follower_count(user)} Followers')
+  print('Waiting to refresh')
+  time.sleep(60)
